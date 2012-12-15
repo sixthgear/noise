9255e9adae5db4ba3cab95083f1a1d1c0ab66a65
sixthgear <sixthgear@sixthgear.ca>
2012-12-15 09:23:56 -0800

    Added test to generate PNG image

--- noise_test.go
+++ noise_test.go
@@ -1,7 +1,11 @@
 package noise
 
+import "os"
 import "fmt"
 import "testing"
+import "image"
+import "image/png"
+import "image/color"
 
 func TestNoise2d(t *testing.T) {
 	fmt.Println("Testing 2D Noise")
@@ -26,4 +30,30 @@ func TestNoise3d(t *testing.T) {
 		fmt.Println()
 	}
 	fmt.Println()
+}
+
+func TestNoisePNG(t *testing.T) {
+
+	fmt.Println("Testing PNG Noise (output to out.png)")
+
+	width, height := 512, 512
+
+	r := image.Rect(0, 0, width, height)
+	i := image.NewRGBA(r)
+	for y := 0; y <= height; y++ {
+		for x := 0; x <= width; x++ {
+			n := Noise2d(float64(x)/float64(width/64), float64(y)/float64(height/64))
+			ni := uint8(n * 255)
+			// fmt.Println(ni)
+			c := color.RGBA{ni, ni, ni, 255}
+			i.SetRGBA(x, y, c)
+		}
+	}
+	f, err := os.Create("out.png")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer f.Close()
+	png.Encode(f, i)
+
 }
